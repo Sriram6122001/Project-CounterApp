6c937ca015fbad0e4d7794272533b7668ee2fa2e
Sriram Srinivas <sriram.s.r@avasoft.com>
2023-10-18 17:00:00 +0530

    stateless-Widget

--- lib/CounterModel.dart
+++ lib/CounterModel.dart
@@ -1,18 +1,13 @@
-
 import 'package:mobx/mobx.dart';
 part 'CounterModel.g.dart';
 
 class CounterModel = _CounterModelBase with _$CounterModel;
-abstract class _CounterModelBase with Store{
+
+abstract class _CounterModelBase with Store {
   //Declare a observable variable with the name counter and initialize the value as 0.
   @observable
-  int counter=0;
-  //create a method with a setCounter with a named parameter with a required variable counter 
-   void setCounter({required int counter})=> this.counter = counter;
+  int counter = 0;
+
+  //create a method with a setCounter with a named parameter with a required variable counter
+  void setCounter({required int counter}) => this.counter = counter;
 }
- 
-
-  
-
-
-
--- lib/CounterViewModel.dart
+++ lib/CounterViewModel.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 import 'CounterModel.dart';
+
 //create a class with a name CounterViewModel which inherits the class CounterModel
-class CounterViewModel extends CounterModel
-{
+class CounterViewModel extends CounterModel {
   /* create a method with a name counterAddFunction */
-    counterAddFunction()
-    {
-      //call the setCounter method and pass the parameter as counter + 1.
-      setCounter(counter: counter+1);
-    }
-    /* create a method with a name counterSubFunction */
-    counterSubFunction()
-    {
-      //call the setCounter method and pass the parameter as counter - 1.
-      setCounter(counter: counter-1);
-    }
+  void counterAddFunction() {
+    //call the setAdditionCounterValue method and pass the parameter as counter + 1.
+    setAdditionCounterValue(value: counter + 1);
+  }
+
+  /* create a method with a name counterSubFunction */
+  void counterSubFunction() {
+    //call the setSubtractionCounterValue method and pass the parameter as counter - 1.
+    setSubtractionCounterValue(value: counter - 1);
+  }
+
+  /* create a method with a name setSubtractionCounterValue and with a parameter named "value" of type integer*/
+  void setSubtractionCounterValue({required int value}) {
+    /* Invoke the setCounter method */
+    setCounter(counter: value);
+  }
+
+  /*create a method with a name setSubtractionCounterValue and with a parameter named "value" of type integer*/
+  void setAdditionCounterValue({required int value}) {
+    /* Invoke the setCounter method */
+    setCounter(counter: value);
+  }
 }
